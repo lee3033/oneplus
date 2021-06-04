f08002a1466d12f3bffc1770c47c253bee424179
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-06-05 03:25:13 +0900

    fix left Dist shape

--- selfdrive/ui/extras.h
+++ selfdrive/ui/extras.h
@@ -12,16 +12,16 @@ static void ui_draw_extras_limit_speed(UIState *s)
 
     if(limit_speed > 10 && left_dist > 0)
     {
-        int w = 200;
-        int h = 200;
-        int x = (s->viz_rect.x + (bdr_s*2)) + 350;
-        int y = 50;
+        int w = 180;
+        int h = 180;
+        int x = (s->viz_rect.x + (bdr_s*2)) + 205;
+        int y = 530;
         char str[32];
 
         nvgBeginPath(s->vg);
-        nvgRoundedRect(s->vg, x, y, w, h, 210);
+        nvgRoundedRect(s->vg, x, y, w, h, 185);
         nvgStrokeColor(s->vg, nvgRGBA(255, 0, 0, 200));
-        nvgStrokeWidth(s->vg, 30);
+        nvgStrokeWidth(s->vg, 15);
         nvgStroke(s->vg);
 
         NVGcolor fillColor = nvgRGBA(0, 0, 0, 50);
@@ -37,14 +37,23 @@ static void ui_draw_extras_limit_speed(UIState *s)
         snprintf(str, sizeof(str), "%d", limit_speed);
         nvgText(s->vg, x+w/2, y+h/2, str, NULL);
 
-        nvgFontSize(s->vg, 120);
+        nvgBeginPath(s->vg);
+        nvgRect(s->vg, x+w/2-100, y+h-40, 200, 80);
+        NVGcolor squareColor = nvgRGBA(255, 0, 0, 200);
+        nvgFillColor(s->vg, squareColor);
+        nvgFill(s->vg);
+        nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
+
+        nvgFontSize(s->vg, 100);
+        nvgFontFace(s->vg, "sans-bold");
+        nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
 
         if(left_dist >= 1000)
             snprintf(str, sizeof(str), "%.1fkm", left_dist / 1000.f);
         else
             snprintf(str, sizeof(str), "%dm", left_dist);
 
-        nvgText(s->vg, x+w/2, y+h + 70, str, NULL);
+        nvgText(s->vg, x+w/2, y+h, str, NULL);
     }
 }
 
